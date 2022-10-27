2484ef294ec57944e72944fbbda1cb78214a0f99
Chad <chadbell045@gmail.com>
2022-10-27 16:20:17 -0600

    Working Commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ class App extends Component {
   state = {
     response: '',
     post: '',
-    responseToPost: '',
+    responseToPost: [],
+    dl: '',
+    en: '',
+    hy: '',
+    ru: '',
+    sp: '',
   };
-  
+
+
   componentDidMount() {
     this.callApi()
       .then(res => this.setState({ response: res.translation }))
@@ -32,20 +38,25 @@ class App extends Component {
       },
       body: JSON.stringify({ post: this.state.post }),
     });
-    const body = await response.text();
-
-    
+    // const body = await response.text();
+    const body = await response.json();
+    console.log(body)
     this.setState({ responseToPost: body });
+    this.setState({ dl: body[0].detectedLanguage.language });
+    this.setState({ en: body[0].translations[0].text });
+    this.setState({ hy: body[0].translations[1].text });
+    this.setState({ ru: body[0].translations[2].text });
+    this.setState({ sp: body[0].translations[3].text });
   };
 
-  
+
 render() {
     return (
       <div className="App">
-        <p>{this.state.response}</p>
+        {/* <p>{this.state.response}</p> */}
         <form onSubmit={this.handleSubmit}>
           <p>
-            <strong>Post to Server:</strong>
+            <strong>Input Text:</strong>
           </p>
           <input
             type="text"
@@ -54,12 +65,31 @@ render() {
           />
           <button type="submit">Submit</button>
         </form>
-        <p>{this.state.responseToPost}</p>
+        {/* <p>{this.state.responseToPost}</p> */}
+            <div>
+            <label for="dl">Detected Language:</label>
+                <span id="dl"> {this.state.dl}</span>
+            </div>
+            <span>
+                <label for="english">English:</label>
+                <span id="english"> {this.state.en}</span>
+            </span>
+            <span>
+                <label for="armenian">Armenian: </label>
+                <span id="armenian"> {this.state.hy}</span>
+            </span>
+            <span>
+                <label for="russian">Russian: </label>
+                <span id="russian"> {this.state.ru}</span>
+            </span>
+            <span>
+                <label for="spanish">Spanish: </label>
+                <span id="spanish"> {this.state.sp}</span>
+            </span>
       </div>
     );
   }
 }
-
 
 
 
